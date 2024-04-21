11bb3670b45d8e53e98abf49cbca2280ce509f78
AsadovIbrahim <ibrahimasadov31@gmail.com>
2024-04-21 20:59:02 +0400

    commit

--- DataBase-Car/obj/Debug/net8.0/DataBase-Car.AssemblyInfo.cs
+++ DataBase-Car/obj/Debug/net8.0/DataBase-Car.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("DataBase-Car")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+29e0463518fb1a085d670293c856930297db94b3")]
 [assembly: System.Reflection.AssemblyProductAttribute("DataBase-Car")]
 [assembly: System.Reflection.AssemblyTitleAttribute("DataBase-Car")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
